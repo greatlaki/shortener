1e76ff30c72e15b3b3604e3edd64912b52ca7a5f
greatlaki <greatlaki911@gmail.com>
2022-07-11 20:25:19 +0300

    Urls model was added

--- src/cutter/models.py
+++ src/cutter/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Urls(models.Model):
+    short_url = models.CharField(max_length=20)
+    long_url = models.URLField('URL', unique=True)
