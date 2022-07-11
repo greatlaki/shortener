8df1fa95132cadb97071d351ac46c96b643cee96
greatlaki <greatlaki911@gmail.com>
2022-07-11 22:21:30 +0300

    urls was updated

--- src/cutter/urls.py
+++ src/cutter/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('<str:token>', home, name='home'),
     path('', make, name='Make new'),
+    path('links/', ListLinks.as_view(), name='list of links'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
