71dafdc0dc6522707f7fa9df4d72cb73a5323dc6
greatlaki <greatlaki911@gmail.com>
2022-07-11 20:30:57 +0300

    Views was added

--- src/cutter/views.py
+++ src/cutter/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import *
+from .shortener import *
+from .models import Urls
+
+
+def home(request, token):
+    long_url = Urls.objects.filter(short_url=token)[0]
+    return redirect(long_url.long_url, )
+
+
+def make(request):
+    form = UrlForm(request.POST)
+    a = ''
+    if request.method == 'POST':
+        if form.is_valid():
+            new_url = form.save(commit=False)
+            a = Shortener().issue_token()
+            new_url.short_url = a
+            new_url.save()
+        else:
+            form = UrlForm()
+            a = 'Invalid URL'
+    return render(request, 'cutter/base.html', {'form': form, 'a': a})
