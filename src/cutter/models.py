9f0850687b013174e68107111ed6d5ff591976ca
greatlaki <greatlaki911@gmail.com>
2022-07-11 22:20:51 +0300

    field 'author' was added

--- src/cutter/models.py
+++ src/cutter/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Urls(models.Model):
     short_url = models.CharField(max_length=20)
     long_url = models.URLField('URL', unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
