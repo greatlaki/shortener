7581a7f73179f03db2ca7eb003931246f221e4ab
greatlaki <greatlaki911@gmail.com>
2022-08-05 23:27:15 +0300

    form was update

--- src/cutter/forms.py
+++ src/cutter/forms.py
@@ -4,6 +4,15 @@ from .models import *
 
 
 class UrlForm(forms.ModelForm):
+
+    long_url = forms.URLField(widget=forms.URLInput(
+        attrs=
+        {
+            "class": "url-form",
+            "placeholder": "Введите URL"
+        }
+    ))
+
     class Meta:
         model = Urls
         fields = ['long_url', 'author']
