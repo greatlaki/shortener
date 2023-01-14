797be14b7fe80b37060fcd1a101f587cc00e1aaf
vadim.rudenkov <vadim.rudenkov@muehlemann-popp.ch>
2023-01-14 14:25:14 +0300

    refactoring

--- src/src/settings.py
+++ src/src/settings.py
@@ -87,17 +87,16 @@ DATABASES = {
     }
 }
 
-if os.environ.get('POSTGRES_NAME'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': env.str('POSTGRES_DB'),
-            'USER': env.str('POSTGRES_USER'),
-            'PASSWORD': env.str('POSTGRES_PASSWORD'),
-            'HOST': env.str('POSTGRES_HOST'),
-            'PORT': env.str('POSTGRES_PORT'),
-        }
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env.str('POSTGRES_DB'),
+#         'USER': env.str('POSTGRES_USER'),
+#         'PASSWORD': env.str('POSTGRES_PASSWORD'),
+#         'HOST': env.str('POSTGRES_HOST'),
+#         'PORT': env.str('POSTGRES_PORT'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
