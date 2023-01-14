6ed48a4e75f537c05b2be4e4fef16afcf6d2a443
vadim.rudenkov <vadim.rudenkov@muehlemann-popp.ch>
2023-01-14 13:42:07 +0300

    add entrypoint

---
+++ bin/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+GUNICORN_TIMEOUT=${GUNICORN_TIMEOUT:-600}
+GUNICORN_WORKERS=${GUNICORN_WORKERS:-2}
+
+python manage.py migrate
+python manage.py runserver
+
+gunicorn --bind 0.0.0.0:8000 app.wsgi --workers $GUNICORN_WORKERS --timeout $GUNICORN_TIMEOUT
