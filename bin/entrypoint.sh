0f1cb46da605832ae96b63ee1e9bea09d50f7ccb
vadim.rudenkov <vadim.rudenkov@muehlemann-popp.ch>
2023-01-14 13:45:26 +0300

    refactoring entrypoint

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -8,4 +8,4 @@ GUNICORN_WORKERS=${GUNICORN_WORKERS:-2}
 python manage.py migrate
 python manage.py runserver
 
-gunicorn --bind 0.0.0.0:8000 app.wsgi --workers $GUNICORN_WORKERS --timeout $GUNICORN_TIMEOUT
+gunicorn --bind 0.0.0.0:8000 src.wsgi --workers $GUNICORN_WORKERS --timeout $GUNICORN_TIMEOUT
