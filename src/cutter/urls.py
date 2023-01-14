a96294ade1d305d212a33a8b9bdb2289deff24a3
vadim.rudenkov <vadim.rudenkov@muehlemann-popp.ch>
2023-01-14 13:31:38 +0300

    refactoring urls

--- src/cutter/urls.py
+++ src/cutter/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
-from .views import *
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('', url_shortener, name='index'),
-    path('view-urls/', view_urls, name='view_urls'),
-    path('register/', register, name='register'),
-    path('<str:shortened_part>', view_shorturl, name='shorturl'),
+    path('', views.index, name='index'),
+    path('shorten', views.shorten_post, name='shorten_post'),
+    path('shorten/<str:url>', views.shorten, name='shorten'),
+    path('<str:url_hash>', views.redirect_hash, name='redirect'),
 ]
