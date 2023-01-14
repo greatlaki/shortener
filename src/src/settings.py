26f5eb0c6df2b596fb55e663f6caa85fd531529a
vadim.rudenkov <vadim.rudenkov@muehlemann-popp.ch>
2023-01-14 14:08:11 +0300

    refactoring

--- src/src/settings.py
+++ src/src/settings.py
@@ -18,11 +18,13 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env.read_env(env.str("ENV_PATH", BASE_DIR.parent / ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
