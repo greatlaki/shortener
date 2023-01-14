bff3571710333ae6cb3424ea1d25cdb371f0e994
vadim.rudenkov <vadim.rudenkov@muehlemann-popp.ch>
2023-01-14 14:36:45 +0300

    refactoring

--- src/cutter/service.py
+++ src/cutter/service.py
@@ -7,7 +7,7 @@ from cutter.models import Urls
 
 def make_shorten(url):
     random_hash = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(7))
-    mapping = Urls(original_url=url, hash=random_hash, creation_date=timezone.now())
+    mapping = Urls(original_url=url, hash=random_hash, created_at=timezone.now())
     mapping.save()
     return random_hash
 
